d43a1572af17dcef8438bdf28881c70a28800fac
FernandoSilva <fernando.andrade.inf@gmail.com>
2020-12-14 10:05:37 -0300

    ultimo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {NgModule , LOCALE_ID} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -18,6 +18,23 @@ import {RedDirective} from './directives/red.directive';
 import {ForDirective} from './directives/for.directive';
 import {ProductCreateComponent} from './components/product/product-create/product-create.component';
 import {MatButtonModule} from "@angular/material/button";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule } from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData} from "@angular/common";
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+
+registerLocaleData(localePt)
 
 @NgModule({
     declarations: [
@@ -30,6 +47,10 @@ import {MatButtonModule} from "@angular/material/button";
         RedDirective,
         ForDirective,
         ProductCreateComponent,
+        ProductReadComponent,
+        ProductRead2Component,
+        ProductUpdateComponent,
+        ProductDeleteComponent,
     ],
     imports: [
         BrowserModule,
@@ -39,9 +60,20 @@ import {MatButtonModule} from "@angular/material/button";
         MatSidenavModule,
         MatListModule,
         MatCardModule,
-        MatButtonModule
+        MatButtonModule,
+        MatSnackBarModule,
+        HttpClientModule,
+        FormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
     ],
-    providers: [],
+    providers: [ {
+        provide: LOCALE_ID,
+        useValue: 'pt-BR'
+    }],
     bootstrap: [AppComponent]
 })
 export class AppModule {
